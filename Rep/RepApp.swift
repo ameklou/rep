e4b16c3f9ab11a5d338dc1d3e36b8395123c249d
Eklou Amemassovor <amekley@gmail.com>
2024-03-30 20:58:14 +0000

    initial commit

---
+++ Rep/RepApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepApp.swift
+//  Rep
+//
+//  Created by Siska on 30/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct RepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
